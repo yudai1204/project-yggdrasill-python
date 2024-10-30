4ef394260dc2371f4f931eed26c394a25a354046
yudai hiyama <yudai1204h@gmail.com>
2024-10-30 20:09:24 +0900

    m5で行けるようにした

--- joroSerial.py
+++ joroSerial.py
@@ -2,26 +2,58 @@ import websockets
 import uuid
 import json
 import asyncio
+import serial
+import time
 
 # WebSocketサーバーのURL
 SERVER_URL = "ws://localhost:3210"
+# SERVER_URL = "wss://project-yggdrasill-backend.icymushroom-df5abb57.japaneast.azurecontainerapps.io"
+
+# シリアルポートの設定
+port = "/dev/cu.M5StickAccel"  # 使用するポート名
+baudrate = 115200  # ボーレートを115200に設定
 
 id = str(uuid.uuid4())
 
+last_send_time = 0
+
 
 async def joro_main(websocket):
-    for i in range(3):
-        await asyncio.sleep(1)
-        # 1秒ごとにjキーが押されているか確認
-        # jが押されていたら、isWateringをTrueにして送信
-        print("sent!")
-        message = json.dumps(
-            {
-                "head": {"type": "joro_status"},
-                "body": {"type": "joro", "uuid": id, "isWatering": True},
-            }
-        )
-        await websocket.send(message)
+    global last_send_time
+    watering_array = []
+    ser = serial.Serial(port, baudrate)
+    try:
+        while True:
+            if ser.in_waiting > 0:
+                data = float(ser.readline().decode("utf-8").rstrip())
+                is_watering_now = data < -0.5
+                # print(is_watering_now)
+                if len(watering_array) > 5:
+                    watering_array.pop(0)
+                watering_array.append(is_watering_now)
+                # 全てTrueなら水やり中
+                is_watering = all(watering_array)
+                print(is_watering)
+
+                if last_send_time + 2 > time.time():
+                    continue
+                if is_watering == False:
+                    continue
+
+                last_send_time = time.time()
+
+                message = json.dumps(
+                    {
+                        "head": {"type": "joro_status"},
+                        "body": {"type": "joro", "uuid": id, "isWatering": is_watering},
+                    }
+                )
+                await websocket.send(message)
+    except KeyboardInterrupt:
+        print("Program interrupted by user.")
+    finally:
+        ser.close()
+        print("Serial port closed.")
 
 
 async def main():
